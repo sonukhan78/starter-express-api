4bfb48b0ec2adeb835ca42c63bc3ba5a2a8f34b7
git config --global user.name islamkhan08 <i6960841@gmail.com>
2023-03-31 10:56:20 +0530

    complete

--- router/intialeTaskrouter/intialerouter.js
+++ router/intialeTaskrouter/intialerouter.js
@@ -2,9 +2,11 @@ const express = require("express");
  
 const Router = express.Router();
 const app = express();
+const protect = require('../../middleware/intialemiddleware/intialemiddleware')
 const { getintiale, postintiale, deleteintiale } = require("../../controller/IntialeTaskcontroller/Intialecontroller");
-const  protect  = require("../../middleware/intialemiddleware/intialemiddleware");
 Router.use(express.json());
+
+
 Router.get("/get",protect,getintiale);
 Router.post("/add",protect, postintiale);
 Router.delete("/:_id",protect, deleteintiale);
