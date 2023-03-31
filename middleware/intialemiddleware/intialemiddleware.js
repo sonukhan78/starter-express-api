cbddb9e9b969de7d12fe33f7eaacb76093b3f660
git config --global user.name islamkhan08 <i6960841@gmail.com>
2023-03-31 16:14:48 +0530

    Todo

--- middleware/intialemiddleware/intialemiddleware.js
+++ middleware/intialemiddleware/intialemiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const user = require("../../module/intialeTaskmodule/intialemodule");
+const intial = require("../../module/intialeTaskmodule/intialemodule");
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
