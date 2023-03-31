eac832a3e9be7f9c18515eac5d85e987ae8ec298
git config --global user.name islamkhan08 <i6960841@gmail.com>
2023-03-31 10:11:45 +0530

    complete

--- controller/IntialeTaskcontroller/Completecontroller.js
+++ controller/IntialeTaskcontroller/Completecontroller.js
@@ -2,30 +2,29 @@ const jwt = require("jsonwebtoken");
 const Complete = require("../../module/IntialeTaskmodule/Completemodule");
 const asyncHandler = require("express-async-handler");
 
-const getcomplete = async(req,res)=>{
-  let data = await Complete.find({ user_id: req.user}) 
-  
-  if(!data.length){
-      res.status(200).json({ status:false, massage: "Complete stage is empty"})
-  }
-  console.log("====>",data);
-  res.status(200).json({ status:true,data})
-  }
+const getcomplete = async (req, res) => {
+  let data = await Complete.find({ user_id: req.user });
 
-  const postcomplete = async(req, res) => {
-    const {title , dis,} = req.body
-    if (!title || !dis) {
-        res.status(400).json({ message: "Please add all Filed" })
-    }
-    let data = await Complete.create({ 
-        title,
-        dis,
-        user_id:req.user
-        
-    });
-    console.log("====>", data); 
-    res.status(200).json(data)
+  if (!data.length) {
+    res.status(200).json({ status: false, massage: "Complete stage is empty" });
   }
+  console.log("====>", data);
+  res.status(200).json({ status: true, data });
+};
+
+const postcomplete = async (req, res) => {
+  const { title, dis } = req.body;
+  if (!title || !dis) {
+    res.status(400).json({ message: "Please add all Filed" });
+  }
+  let data = await Complete.create({
+    title,
+    dis,
+    user_id: req.user,
+  });
+  console.log("====>", data);
+  res.status(200).json(data);
+};
 
 const deletecomplete = asyncHandler(async (req, res) => {
   let findid = await Complete.findById(req.params._id);
